46914dd4e1131aee8642b4442c37a4651a9da9a1
lijason103 <lijason103@gmail.com>
2019-02-09 10:56:38 -0800

    Added map for client

--- public/js/game.js
+++ public/js/game.js
@@ -21,23 +21,54 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
+    // Load map tiles
+    this.load.image('ground', 'assets/ground.png')
+    this.load.image('wall', 'assets/wall.png')
+
+    // Load players
 }
 
 function create() {
+    let sceneWidth = this.game.config.width
+    let sceneHeight = this.game.config.height
     this.socket = io()
     this.socket.on('disconnect', id => {
         // Remove player and player sprite
     })
 
     this.socket.on('map', map => {
-        console.log(map)
         // Set the block size
-        this.blockWidth = width / map[0].length
-        this.blockHeight = height / map.length
+        this.blockHeight = sceneHeight / map.length
+        this.blockWidth = sceneWidth / map[0].length
         this.map = map
+        renderMap(this)
     })
-    
 }
 
 function update(time, delta) {
 }
+
+function renderMap(scene) {
+    let map = scene.map
+    for (let row = 0; row < map.length; ++row) {
+        for (let column = 0; column < map[row].length; ++column) {
+            let x = column * scene.blockWidth + scene.blockWidth / 2
+            let y = row * scene.blockHeight + scene.blockHeight / 2
+            let tile = map[row][column]
+            let image
+            // render wall
+            if (tile == 'w') {
+                image = scene.add.sprite(x, y, 'wall')
+                image.displayWidth = scene.blockWidth
+                image.displayHeight = scene.blockHeight
+            }
+            // render floor
+            if (tile == 'e') {
+                image = scene.add.sprite(x, y, 'ground')
+                image.displayWidth = scene.blockWidth
+                image.displayHeight = scene.blockHeight
+            }
+        }
+    }    
+}
+
